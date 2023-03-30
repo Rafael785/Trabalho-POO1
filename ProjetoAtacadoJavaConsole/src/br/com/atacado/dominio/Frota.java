c3d4321b16834a109c60f978ac5f07e291ae9631
Rafael785 <rafaelbandms@gmail.com>
2023-03-30 19:04:05 -0400

    semi=pronto

--- ProjetoAtacadoJavaConsole/src/br/com/atacado/dominio/Frota.java
+++ ProjetoAtacadoJavaConsole/src/br/com/atacado/dominio/Frota.java
@@ -2,4 +2,18 @@ package br.com.atacado.dominio;
 
 public class Frota {
 
+    protected int codigofrota;
+
+    public int getCodigofrota() {
+        return codigofrota;
+    }
+
+    public void setCodigofrota(int codigofrota) {
+        this.codigofrota = codigofrota;
+    }
+
+    public Frota(int codigofrota) {
+        this.codigofrota = codigofrota;
+    }
+
 }
